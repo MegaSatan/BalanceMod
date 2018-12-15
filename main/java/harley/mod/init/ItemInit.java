55c6e4c5c9850f4061d28bfe6093e3c8bdd6561b
MegaSatan <45887517+MegaSatan@users.noreply.github.com>
2018-12-15 01:00:01 -0600

    Add files via upload

---
+++ main/java/harley/mod/init/ItemInit.java
@@ -0,0 +1,13 @@
+package harley.mod.init;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import harley.mod.objects.items.ItemBase;
+import net.minecraft.item.Item;
+
+public class ItemInit {
+	public static final List<Item> ITEMS = new ArrayList<Item>();
+	
+	public static final Item INGOT_SILVER = new ItemBase("ingot_silver");
+}
---
+++ main/java/harley/mod/util/IHasModel.java
@@ -0,0 +1,5 @@
+package harley.mod.util;
+
+public interface IHasModel {
+	public void registerModels();
+}
